a9e268c8798350f3c3ccf44bd8b6a29760f2e668
AnmolDevero <anmoldevero974@gmail.com>
2025-08-21 09:25:32 +0500

    Add Task API views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+class TaskListCreateView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetailView(APIView):
+    def get_obj(self, id):
+        try:
+            return Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            return None
+        
+    def get(self, request, id):
+        task = self.get_obj(id)
+        if not task:
+            return Response({'error':'task not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        task = self.get_obj(id)
+        if not task:
+            return Response({'error':'task not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        task = self.get_obj(id)
+        if not task:
+            return Response({'error':'task not found'}, status=status.HTTP_404_NOT_FOUND)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
